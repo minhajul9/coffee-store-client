7190c292146a12911ea5d2efa3f6311519f37df7
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-05-11 21:32:50 +0600

    add to db and get from db

---
+++ src/component/AddCoffee.jsx
@@ -0,0 +1,126 @@
+import Swal from 'sweetalert2'
+
+const AddCoffee = () => {
+
+    const handelAddCoffee = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const quantity = form.quantity.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+
+        const newCoffee = { name, quantity, supplier, taste, category, details, photo }
+        console.log(newCoffee);
+        fetch('http://localhost:5000/coffee', {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(newCoffee)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Coffee added successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    })
+                    form.reset()
+                }
+            })
+    }
+
+    return (
+        <div className='bg-[#F4F3F0] p-32'>
+            <h1 className='text-6xl text-purple-600'>Vite + React</h1>
+            <form onSubmit={handelAddCoffee}>
+
+                {/* form name and quantity row */}
+                <div className='md:flex mb-8'>
+                    <div className="form-control w-1/2">
+                        <label className="label">
+                            <span className="label-text">Coffee Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name='name' placeholder="Coffee Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Available Quantity</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name='quantity' placeholder="Available Quantity" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+
+                {/* form supplier and taste row */}
+                <div className='md:flex mb-8'>
+                    <div className="form-control w-1/2">
+                        <label className="label">
+                            <span className="label-text">Supplier</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name='supplier' placeholder="Supplier" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Taste</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name='taste' placeholder="Taste" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+
+                {/* form category and details row */}
+                <div className='md:flex mb-8'>
+                    <div className="form-control w-1/2">
+                        <label className="label">
+                            <span className="label-text">Category</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name='category' placeholder="Category" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Details</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name='details' placeholder="Details" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+
+                {/* form photo row */}
+                <div className='md:flex mb-8'>
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name='photo' placeholder="Photo URL" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+
+
+                <input type="submit" value="Add Coffee" className="btn btn-block" />
+            </form>
+
+        </div>
+    );
+};
+
+export default AddCoffee;
--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,23 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import AddCoffee from './component/AddCoffee.jsx';
+import UpdateCoffee from './component/UpdateCoffee.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    loader: () => fetch('http://localhost:5000/coffee')
   },
+  {
+    path: 'addCoffee',
+    element: <AddCoffee></AddCoffee>
+  },
+  {
+    path: 'updateCoffee',
+    element: <UpdateCoffee></UpdateCoffee>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
